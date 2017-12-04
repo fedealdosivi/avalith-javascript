7d63597f07cee993719fc5caebaf514fa95a5493
Federico Palomero Garcia <fedepalo13@gmail.com>
2017-12-04 11:20:15 -0300

    side bar and card added

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,21 +1,33 @@
 <template>
     <div class="app">
         <top-menu></top-menu>
+        <side-bar :isMenuOpen="isMenuOpen"></side-bar>
+        <tec-card></tec-card>
     	<router-view></router-view>
     </div>
 </template>
 
 <script>
     import topMenu from './top-menu.vue';
+    import sideBar from './side-bar.vue';
+    import tecCard from './tec-card.vue';
 
     export default {
         name: 'app',
         components: {
-            topMenu
+            topMenu,
+            sideBar,
+            tecCard
         },
         data() {
             return {
-
+                isMenuOpen: true,
+            }
+        },
+        methods: {
+            toggleMenu(payload) {
+                console.log(payload);
+                this.isMenuOpen = !this.isMenuOpen;
             }
         }
     }
---
+++ src/components/side-bar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="isMenuOpen" class="side-menu">
+    	
+    </div>
+</template>
+
+<script>
+    export default {
+    	name: 'sideMenu',
+    	props: ['isMenuOpen']
+    }
+</script>
+
+<style>
+.side-menu {
+	position: fixed;
+	top: 0;
+	left: 0;
+	padding: 10px;
+	width: 300px;
+	height: 708px;
+	background-color: black;
+	z-index: 50;
+}
+</style>
